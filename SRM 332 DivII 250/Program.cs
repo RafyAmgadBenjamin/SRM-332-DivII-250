4a1442bda67c9e0ed989ae1086ce968e453fd3a3
Rafy Amgad Benjamin <rafy.amgadbenjamin@gmail.com>
2019-02-25 23:02:21 +0200

    After submitted succufully

--- SRM 332 DivII 250/Program.cs
+++ SRM 332 DivII 250/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SRM_332_DivII_250
 {
@@ -10,6 +7,41 @@ namespace SRM_332_DivII_250
     {
         static void Main(string[] args)
         {
+            TextStatistics test = new TextStatistics();
+            Console.WriteLine(test.averageLength("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+        }
+    }
+
+    class TextStatistics
+    {
+        public double averageLength(String text)
+        {
+            char[] delimeters = new char[] { ',', '.', '?', '!', '-', ' ' };
+            int eachWordCount = 0;
+            int totalTextCount = 0;
+            int noOfWordsInText = 0;
+            text = text.ToLower();
+            if (text == "")
+                return 0;
+            text += ' ';
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (delimeters.Contains(text[i]) || (text[i] <= 'a' && text[i] >= 'z')) // word has finished
+                {
+                    totalTextCount += eachWordCount;
+                    if (eachWordCount != 0)
+                    {
+                        noOfWordsInText++;
+                    }
+                    eachWordCount = 0;
+                }
+                else//still in a word
+                {
+                    eachWordCount++;
+                }
+            }
+            return totalTextCount / (noOfWordsInText * 1.0);
         }
     }
 }
